854a61a7784694aabd193cbb5650d9a9e8c782ee
Luis Hernandez <lhernandezd12@miumg.edu.gt>
2025-05-21 17:07:37 -0600

    CARGA INICIAL DEL PROYECTO DESDE NETBEANS

---
+++ src/main/java/com/proyecto/reservasprioridad/ReservasprioridadApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.reservasprioridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasprioridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasprioridadApplication.class, args);
+	}
+
+}
